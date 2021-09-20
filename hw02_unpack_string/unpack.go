e85a3fd6af16a6e9636c8a1759e459b214e6e7e7
Tawr <see.fill@yandex.ru>
2021-09-20 23:06:10 +0300

    some lint fix

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -13,17 +13,17 @@ func Unpack(inp string) (string, error) {
 	var strBldr strings.Builder
 	runes := []rune(inp)
 	for i := 0; i < len(runes); i++ {
-		//Check for backslas
+		// Check for backslas
 		if runes[i] == 92 {
 			i++
 			strBldr.WriteRune(runes[i])
 		} else if rep, err := strconv.Atoi(string(runes[i])); err == nil {
-			//Check for non-digit input
+			// Check for non-digit input
 			if (i > 0 && unicode.IsDigit(runes[i-1]) && (i > 1 && runes[i-2] != 92)) || i == 0 {
 				return "", ErrInvalidString
 			}
-			//Check for zero digit
-			rep = rep - 1
+			// Check for zero digit
+			rep--
 			if rep > 0 {
 				strBldr.WriteString(strings.Repeat(string(runes[i-1]), rep))
 			} else {
@@ -32,7 +32,6 @@ func Unpack(inp string) (string, error) {
 				strBldr.Reset()
 				strBldr.WriteString(tempStr)
 			}
-
 		} else {
 			strBldr.WriteRune(runes[i])
 		}
