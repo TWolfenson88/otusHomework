7ff1f4cdbf620eebc44f52a06b156c5676319323
Tawr <see.fill@yandex.ru>
2021-09-20 22:37:31 +0300

    upgrade func and tests

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -10,61 +10,32 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(inp string) (string, error) {
-
-	//Check condition if input string is a number
-	_, errChk := strconv.Atoi(inp)
-	if errChk == nil {
-		return "", ErrInvalidString
-	}
-
-	//Check condition if first char is a number
-	for idx, char := range inp {
-		if unicode.IsDigit(char) && idx == 0 {
-			return "", ErrInvalidString
-		}
-	}
-
-	//Check condition if input sting contian not a digit
-	prevChar := 'a'
-	for _, char := range inp {
-		if unicode.IsDigit(char) && unicode.IsDigit(prevChar) {
-			return "", ErrInvalidString
-		}
-		prevChar = char
-	}
-
-	outStr := ""
-
-	// charr := ' '
-	prevChar = ' '
-
-	//a4bc5      aab5e
-
-	for _, char := range inp {
-
-		if unicode.IsLetter(prevChar) && unicode.IsDigit(char) {
-			rep, err := strconv.Atoi(string(char))
-			if err != nil {
+	var strBldr strings.Builder
+	runes := []rune(inp)
+	for i := 0; i < len(runes); i++ {
+		//Check for backslas
+		if runes[i] == 92 {
+			i++
+			strBldr.WriteRune(runes[i])
+		} else if rep, err := strconv.Atoi(string(runes[i])); err == nil {
+			//Check for non-digit input
+			if (i > 0 && unicode.IsDigit(runes[i-1]) && (i > 1 && runes[i-2] != 92)) || i == 0 {
 				return "", ErrInvalidString
 			}
+			//Check for zero digit
 			rep = rep - 1
 			if rep > 0 {
-				outStr += strings.Repeat(string(prevChar), rep)
-			} else if rep < 0 {
-				outStr = strings.Replace(outStr, string(prevChar), "", 1)
+				strBldr.WriteString(strings.Repeat(string(runes[i-1]), rep))
+			} else {
+				tempStr := strBldr.String()
+				tempStr = strings.Replace(tempStr, string(runes[i-1]), "", 1)
+				strBldr.Reset()
+				strBldr.WriteString(tempStr)
 			}
-			prevChar = char
-		} else if unicode.IsDigit(prevChar) && unicode.IsLetter(char) {
-			outStr += string(char)
-			prevChar = char
-		} else if unicode.IsLetter(prevChar) && unicode.IsLetter(char) {
-			outStr += string(char)
-			prevChar = char
-		} else if prevChar == ' ' {
-			outStr += string(char)
-			prevChar = char
+
+		} else {
+			strBldr.WriteRune(runes[i])
 		}
 	}
-
-	return outStr, nil
+	return strBldr.String(), nil
 }
