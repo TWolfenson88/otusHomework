e0bfb6b517d46b2761b1ef2e058b7f525618c972
Tawr <see.fill@yandex.ru>
2021-09-21 22:22:58 +0300

    fix last linter error

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -9,30 +9,66 @@ import (
 
 var ErrInvalidString = errors.New("invalid string")
 
+func checkErrors(inp string) bool {
+	runes := []rune(inp)
+
+	// Check condition if input string is a number
+	_, errChk := strconv.Atoi(inp)
+	if errChk == nil {
+		return true
+	}
+
+	// Check condition if first char is a number
+	for idx, char := range inp {
+		if unicode.IsDigit(char) && idx == 0 {
+			return true
+		}
+	}
+
+	for i := 2; i < len(runes); i++ {
+		if unicode.IsDigit(runes[i]) && unicode.IsDigit(runes[i-1]) && (i > 1 && runes[i-2] != 92) {
+			return true
+		}
+	}
+	return false
+}
+
+func repeater(rep int, str string, run rune) string {
+	var strBldr strings.Builder
+	if rep > 0 {
+		strBldr.WriteString(str)
+		strBldr.WriteString(strings.Repeat(string(run), rep))
+		str = strBldr.String()
+	} else {
+		str = strings.Replace(str, string(run), "", 1)
+	}
+	return str
+}
+
 func Unpack(inp string) (string, error) {
 	var strBldr strings.Builder
 	runes := []rune(inp)
+
+	if checkErrors(inp) {
+		return "", ErrInvalidString
+	}
+
 	for i := 0; i < len(runes); i++ {
 		// Check for backslas
-		if runes[i] == 92 {
+
+		switch rn := runes[i]; {
+		case rn == 92:
 			i++
 			strBldr.WriteRune(runes[i])
-		} else if rep, err := strconv.Atoi(string(runes[i])); err == nil {
-			// Check for non-digit input
-			if (i > 0 && unicode.IsDigit(runes[i-1]) && (i > 1 && runes[i-2] != 92)) || i == 0 {
-				return "", ErrInvalidString
-			}
-			// Check for zero digit
+		case unicode.IsDigit(rn):
+			rep, _ := strconv.Atoi(string(runes[i]))
 			rep--
-			if rep > 0 {
-				strBldr.WriteString(strings.Repeat(string(runes[i-1]), rep))
-			} else {
-				tempStr := strBldr.String()
-				tempStr = strings.Replace(tempStr, string(runes[i-1]), "", 1)
-				strBldr.Reset()
-				strBldr.WriteString(tempStr)
-			}
-		} else {
+
+			tempStr := strBldr.String()
+			tempStr = repeater(rep, tempStr, runes[i-1])
+			strBldr.Reset()
+			strBldr.WriteString(tempStr)
+		default:
 			strBldr.WriteRune(runes[i])
 		}
 	}
