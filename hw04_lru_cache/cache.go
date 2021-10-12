28c1e3e8457c2081d7ed61a666ba6f8fa4564251
Tawr <see.fill@yandex.ru>
2021-10-12 22:11:57 +0300

    hw04 complete

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,15 +9,13 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
 }
 
 type cacheItem struct {
-	key   string
+	key   Key
 	value interface{}
 }
 
@@ -26,5 +24,47 @@ func NewCache(capacity int) Cache {
 		capacity: capacity,
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
+	}
+}
+
+func (lc lruCache) Set(key Key, value interface{}) bool {
+	if lc.capacity == 0 {
+		return false
+	}
+
+	if _, ok := lc.items[key]; ok {
+		lc.items[key].Value.(*cacheItem).value = value
+		lc.queue.MoveToFront(lc.items[key])
+		lc.items[key] = lc.queue.Front()
+		return ok
+	}
+
+	lc.queue.PushFront(&cacheItem{
+		key:   key,
+		value: value,
+	})
+	lc.items[key] = lc.queue.Front()
+
+	if lc.queue.Len() > lc.capacity {
+		lc.Clear()
+	}
+	return false
+}
+
+func (lc lruCache) Get(key Key) (interface{}, bool) {
+	if _, ok := lc.items[key]; ok {
+		lc.queue.MoveToFront(lc.items[key])
+		lc.items[key] = lc.queue.Front()
+		return lc.items[key].Value.(*cacheItem).value, ok
+	}
+	return nil, false
+}
+
+func (lc lruCache) Clear() {
+	// lastItem := lc.queue.Back()
+
+	if item, ok := lc.queue.Back().Value.(*cacheItem); ok {
+		delete(lc.items, item.key)
+		lc.queue.Remove(lc.queue.Back())
 	}
 }
