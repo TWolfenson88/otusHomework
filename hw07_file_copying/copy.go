fb9883fd690c5a355875d1fa0669ce2d9e048345
Tawr <see.fill@yandex.ru>
2021-11-29 21:31:48 +0300

    hw07 complete

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"errors"
+	"io"
+	"math"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -9,7 +14,66 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
+const MB = 1024 * 1024
+
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fStat, err := os.Stat(fromPath)
+	if err != nil {
+		return err
+	}
+	if !fStat.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+
+	fSize := fStat.Size()
+	if fSize <= offset {
+		return ErrOffsetExceedsFileSize
+	}
+	if limit == 0 {
+		limit = fSize - offset
+	}
+
+	in, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	out, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = in.Seek(offset, io.SeekStart)
+	if err != nil {
+		return err
+	}
+
+	bytesCopy := limit
+	readSize := int64(math.Min(float64(MB), float64(limit)))
+	var totalReadBytes int64
+
+	bar := pb.Full.Start64(bytesCopy)
+	fToBarProxy := bar.NewProxyWriter(out)
+
+	for {
+		n, err := io.CopyN(fToBarProxy, in, readSize)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+
+			return err
+		}
+
+		totalReadBytes += n
+		if totalReadBytes >= bytesCopy {
+			break
+		}
+	}
+
+	bar.Finish()
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,96 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	inputFile  = "./testdata/input.txt"
+	outputFile = "./testdata/output.txt"
+)
+
+func readFromFiles(t *testing.T, expFile string) ([]byte, []byte) {
+	t.Helper()
+	resultText, err := ioutil.ReadFile(outputFile)
+	require.NoError(t, err)
+
+	expectedText, err := ioutil.ReadFile(expFile)
+	require.NoError(t, err)
+
+	return resultText, expectedText
+}
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Cleanup(func() {
+		err := os.Remove(outputFile)
+		require.NoError(t, err)
+	})
+
+	t.Run("offset 0, limit 0", func(t *testing.T) {
+		err := Copy(inputFile, outputFile, 0, 0)
+		require.NoError(t, err)
+
+		resultText, expectedText := readFromFiles(t, "./testdata/out_offset0_limit0.txt")
+
+		require.Equal(t, expectedText, resultText)
+	})
+
+	t.Run("offset 0, limit 1000", func(t *testing.T) {
+		err := Copy(inputFile, outputFile, 0, 1000)
+		require.NoError(t, err)
+
+		resultText, expectedText := readFromFiles(t, "./testdata/out_offset0_limit1000.txt")
+
+		require.Equal(t, string(expectedText), string(resultText))
+	})
+
+	t.Run("offset 100, limit 1000", func(t *testing.T) {
+		err := Copy(inputFile, outputFile, 100, 1000)
+		require.NoError(t, err)
+
+		resultText, expectedText := readFromFiles(t, "./testdata/out_offset100_limit1000.txt")
+
+		require.Equal(t, string(expectedText), string(resultText))
+	})
+
+	t.Run("offset 6000, limit 1000", func(t *testing.T) {
+		err := Copy(inputFile, outputFile, 6000, 1000)
+		require.NoError(t, err)
+
+		resultText, expectedText := readFromFiles(t, "./testdata/out_offset6000_limit1000.txt")
+
+		require.Equal(t, string(expectedText), string(resultText))
+	})
+
+	t.Run("Error: offset more than file size", func(t *testing.T) {
+		err := Copy(inputFile, outputFile, 0, 0)
+		require.NoError(t, err)
+
+		require.Error(t, ErrOffsetExceedsFileSize, err)
+	})
+
+	t.Run("Error: file with zero size", func(t *testing.T) {
+		err := Copy("/dev/urandom", outputFile, 0, 0)
+
+		require.Error(t, ErrUnsupportedFile, err)
+	})
+
+	t.Run("Limit more than file. Read until EOF", func(t *testing.T) {
+		err := Copy(inputFile, outputFile, 0, 9999999999)
+		require.NoError(t, err)
+
+		resultText, expectedText := readFromFiles(t, "./testdata/out_offset0_limit0.txt")
+
+		require.Equal(t, string(expectedText), string(resultText))
+	})
+
+	t.Run("Should return nil if all are ok", func(t *testing.T) {
+		err := Copy(inputFile, outputFile, 0, 0)
+
+		require.Nil(t, err)
+	})
 }
